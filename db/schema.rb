7938d67231862576a691459611331f88324dac66
Spencer Williams <s@spencerenglish.com>
2010-01-03 05:32:48 +0000

    finishing contenttype schema thing

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091028180014) do
+ActiveRecord::Schema.define(:version => 20100103053236) do
 
   create_table "contents", :force => true do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20091028180014) do
     t.string   "contentpane"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "contenttype"
   end
 
   create_table "panes", :force => true do |t|
