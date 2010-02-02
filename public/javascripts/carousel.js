bacde3f07d489d3ae2622a5d5fbd84160c1d784c
Spencer Williams <s@spencerenglish.com>
2010-02-02 16:11:31 +0000

    redesign round 7. carousel engine!

---
+++ public/javascripts/carousel.js
@@ -0,0 +1,39 @@
+function left() {
+  jQuery('#carousellinkscontainer')[0].style.left = navleft+"px";
+  var x = jQuery("#carousellinkscontainer")[0].style.left;
+  if (x == "") x = "0px";
+  var y = parseInt(x.substring(0,x.indexOf("px")));
+  y += 165;
+  //console.log(x,y);
+  x = y+"px";
+  if (y <= 0) {
+    navleft = y;
+    jQuery("#carousellinkscontainer").animate({left: x},"fast");
+  }
+}
+
+function right() {
+  jQuery('#carousellinkscontainer')[0].style.left = navleft+"px";
+  var navmin = (jQuery('#carousellinkscontainer > div').size()-2) * -165;
+  var x = jQuery("#carousellinkscontainer")[0].style.left;
+  if (x == "") x = "0px";
+  var y = parseInt(x.substring(0,x.indexOf("px")));
+  y -= 165;
+  //console.log(x,y);
+  x = y+"px";
+  if (y > navmin) {
+    navleft = y;
+    jQuery("#carousellinkscontainer").animate({left: x},"fast");
+  }
+}
+
+function carousel(abbrev) {
+  for (i=0; i<jQuery('#carouselitemscontainer > div').size(); i++) {
+    if (jQuery('#carouselitemscontainer > div')[i].id == "carouselitem_"+abbrev)
+      break;
+  }
+  x = i*-511;
+  y = x+"px";
+  jQuery("#carouselitemscontainer").animate({left: y},"fast");
+  //console.log(i);
+}
