0232d7e797f8f8ff3a6f3dd8b578f4e296c90c02
Spencer Williams <s@spencerenglish.com>
2010-03-03 00:28:50 +0000

    actually adding some tickets

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,20 @@
+class TicketsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def createticketrez
+  end
+
+  def createticketalert
+  end
+
+  def editticketrez
+  end
+
+  def removeticketalert
+  end
+
+end
