f6e633ca5e8e9e1d9ef6df5350456975ac51e8e0
Spencer Williams <s@spencerenglish.com>
2010-03-11 07:29:20 +0000

    fixing roles

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100311041340) do
+ActiveRecord::Schema.define(:version => 20100311240722) do
 
   create_table "contents", :force => true do |t|
     t.string   "title"
