877f3e6e1cb1c9daa84f26604fc3adb334576d5e
Spencer Williams <s@spencerenglish.com>
2010-03-27 06:32:39 +0000

    small things

--- app/controllers/boxoffice_controller.rb
+++ app/controllers/boxoffice_controller.rb
@@ -3,14 +3,14 @@ class BoxofficeController < AdminController
   def index
     shows = Show.all
     shows.sort! { |x, y| Time.parse(x.performancetimes.split("|")[0])<=>Time.parse(y.performancetimes.split("|")[0]) }
-    shows.reverse.each { |show| show = show unless !show.upcoming }
-    redirect_to :controller => "boxoffice", :action => "show", :abbrev => show.abbrev unless show.nil?
+    shows.reverse.each { |s| @show = s unless !s.upcoming }
+    redirect_to :controller => "boxoffice", :action => "show", :abbrev => @show.abbrev unless @show.nil?
   end
 
   def show
     shows = Show.all
     shows.sort! { |x, y| Time.parse(x.performancetimes.split("|")[0])<=>Time.parse(y.performancetimes.split("|")[0]) }
-    shows.reverse.each { |show| @show = show unless !show.upcoming }
+    shows.reverse.each { |s| @show = s unless !s.upcoming }
     @perfs = @show.performancetimes.split("|")
     @sections = Ticketsection.all(:conditions => ["showid = ?",@show.abbrev])
   end
