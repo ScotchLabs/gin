ad2d173302f146bd7ef14e91507509e0a9041b97
Spencer Williams <s@spencerenglish.com>
2010-04-02 16:04:21 +0000

    roles != roleassocs

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,3 @@
 class Role < ActiveRecord::Base
   has_many :roleassocs
-  
-  validates_inclusion_of :userid, :in => User.all.map{|u| u.name}
-  validates_inclusion_of :roleid, :in => Role.all.map{|r| r.rabbrev}
 end
