f12eebb1e718a2c6c5b2a6be4142055a94c90ef5
Spencer Williams <s@spencerenglish.com>
2010-04-02 15:57:42 +0000

    oops

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :roleassocs
   
-  validate_inclusion_of :userid, :in => User.all.map{|u| u.name}
-  validate_inclusion_of :roleid, :in => Role.all.map{|r| r.rabbrev}
+  validates_inclusion_of :userid, :in => User.all.map{|u| u.name}
+  validates_inclusion_of :roleid, :in => Role.all.map{|r| r.rabbrev}
 end
