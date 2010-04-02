fee5fbeb1620ce3af5871f2098c420f79c41373a
Spencer Williams <s@spencerenglish.com>
2010-04-02 15:56:48 +0000

    roles validation, updates expiration

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :roleassocs
+  
+  validate_inclusion_of :userid, :in => User.all.map{|u| u.name}
+  validate_inclusion_of :roleid, :in => Role.all.map{|r| r.rabbrev}
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -1,11 +1,14 @@
 class Update < ActiveRecord::Base
-  named_scope :not_expired, :conditions => ['expiredate > ?', DateTime.now], :order => "created_at DESC"
 
   validates_presence_of :name, :anchor, :expiredate
   validates_uniqueness_of :anchor
   # validate expiredate is in the future
   validate :expiredate_in_future
   validate :article_ok
+
+  def expired
+    DateTime.now.strftime("%Y-%m-%d %H:%M:%S %z") > expiredate.strftime("%Y-%m-%d %H:%M:%s %z")
+  end
 
   def articletext
     RedCloth.new(article).to_html
