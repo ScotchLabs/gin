ee2db72d10f526925bb821f17b421ebd992a32ac
Spencer Williams <s@spencerenglish.com>
2010-09-08 18:00:25 +0000

    fixes carousel times

--- app/models/show.rb
+++ app/models/show.rb
@@ -95,12 +95,14 @@ class Show < ActiveRecord::Base
   
   def perfcarousel
     perfarr = performancetimes.split("|")
-    out = ""
     dayarr = Array.new
     for perf in perfarr
       t = Time.parse(perf)
       d = Time.local(t.year, t.month, t.day)
-      t = t.hour
+      tstr = "#{t.hour}"
+      if t.min > 0
+        tstr = "#{tstr}:#{t.min.to_s}"
+      end
       daypresent = false
       for day in dayarr
         if day[0] == d
@@ -109,22 +111,30 @@ class Show < ActiveRecord::Base
         end
       end
       if daypresent
-        day[1].push(t)
+        day[1].push(tstr)
       else
-        dayarr.push([d,[t]])
+        dayarr.push([d,[tstr]])
       end
     end
+    out = ""
     for day in dayarr
-      out += "<span class='upper'>";
+      out += "<span class='upper'>"
       out += Time.at(day[0]).strftime("%b %d").to_s
-      out += "</span>&nbsp;&nbsp;&nbsp;&nbsp;@ ";
+      out += "</span>&nbsp;&nbsp;&nbsp;&nbsp;@ "
       for time in day[1]
         if day[1].size() > 2 && day[1].index(time)<day[1].size()-2
           out += ", "
         elsif day[1].size() > 1 && day[1].index(time)==day[1].size()-1
           out += " & "
         end
-        out += (time%12+((time==0)? 12:0)).to_s+((time>11)? "p.":"a.")
+        if time.index(":")
+          hour = time[0...time.index(":")].to_i
+        else
+          hour = time.to_i
+        end
+        milhour = hour
+        hour = (hour%12+((time==0)? 12:0))
+        out += "#{hour.to_s}#{(time.index ':')? (time[time.index(':')+1..time.length]):('')}#{(milhour>11)? ("p."):("a.")}"
       end
       out += "<br />"
     end
